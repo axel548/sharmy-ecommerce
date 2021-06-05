4d561bf164b18e51588753193f8ff14161a061a6
axel <axel.aatl@gmail.com>
2021-06-05 12:04:35 -0400

    IInstalar Angular Firebase y configurar Firebase Auth, Implementación Auth y Guards, Subiendo una imagen a Firebase Storage

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFireStorage } from '@angular/fire/storage';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { finalize } from 'rxjs/operators';
 import { ProductsService } from 'src/app/core/services/products/products.service';
 import {MyValidators} from './../../../utils/validators'
 
@@ -12,11 +15,13 @@ import {MyValidators} from './../../../utils/validators'
 export class FormProductComponent implements OnInit {
 
   form!: FormGroup;
+  image$!: Observable<any>;
 
   constructor(
     private formBuilder: FormBuilder,
     private productsService: ProductsService,
-    private router:Router
+    private router:Router,
+    private storage: AngularFireStorage
   ) { 
     this.buildForm()
   }
@@ -48,6 +53,24 @@ export class FormProductComponent implements OnInit {
 
   get priceField(){
     return this.form.get('price')
+  }
+
+  uploadFile(event: any) {
+      const file = event.target.files[0]
+      const name = 'image.png';
+      const fileRef = this.storage.ref(name);
+      const task = this.storage.upload(name, file)
+
+      task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL()
+          this.image$.subscribe(url => {
+            this.form.get('image')?.setValue(url)
+          })
+        })
+      )
+      .subscribe()
   }
 
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,15 @@
 
 export const environment = {
   production: false,
-  url_api: 'https://platzi-store.herokuapp.com/products/'
+  url_api: 'https://platzi-store.herokuapp.com/products/',
+  firebaseConfig : {
+    apiKey: "AIzaSyCqM_tOY7VGrZI4v1WXfqhUZBX9Xz7KCLw",
+    authDomain: "platzi-store-44b68.firebaseapp.com",
+    projectId: "platzi-store-44b68",
+    storageBucket: "platzi-store-44b68.appspot.com",
+    messagingSenderId: "496962645679",
+    appId: "1:496962645679:web:958a10fdbc65bc13986e5a"
+  }
 };
 
 /*
